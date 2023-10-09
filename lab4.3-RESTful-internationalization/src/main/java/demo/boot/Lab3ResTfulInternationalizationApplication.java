3ee9f6d1d586e44858aab4e5c0f9c109ee766ace
jbossramana <jbossramana@gmail.com>
2023-10-09 12:04:26 +0530

    Update Lab3ResTfulInternationalizationApplication.java

--- lab4.3-RESTful-internationalization/src/main/java/demo/boot/Lab3ResTfulInternationalizationApplication.java
+++ lab4.3-RESTful-internationalization/src/main/java/demo/boot/Lab3ResTfulInternationalizationApplication.java
@@ -1,7 +1,13 @@
 package demo.boot;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class Lab3ResTfulInternationalizationApplication {
@@ -10,4 +16,23 @@ public class Lab3ResTfulInternationalizationApplication {
 		SpringApplication.run(Lab3ResTfulInternationalizationApplication.class, args);
 	}
 
+
+	@Bean
+	public  LocaleResolver   localeResolver()
+	{
+	SessionLocaleResolver  localResolver = new SessionLocaleResolver();
+	localResolver.setDefaultLocale(Locale.US);
+	return localResolver;
+	}
+
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource  = new ResourceBundleMessageSource();
+	     
+	    messageSource.setBasename("messages");
+	    return messageSource;
+	}
+
+	
 }
