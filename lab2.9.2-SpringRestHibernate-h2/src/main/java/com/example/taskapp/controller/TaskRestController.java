5af12c3122f9a2f9db804e8e9fa4ea7fef703f11
jbossramana <jbossramana@gmail.com>
2025-09-16 16:50:51 +0530

    Update TaskRestController.java

--- lab2.9.2-SpringRestHibernate-h2/src/main/java/com/example/taskapp/controller/TaskRestController.java
+++ lab2.9.2-SpringRestHibernate-h2/src/main/java/com/example/taskapp/controller/TaskRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/task")
 public class TaskRestController {
 
     @Autowired
