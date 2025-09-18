48a24e6921f833c8dd08eccb7056e1a10497eb15
jbossramana <jbossramana@gmail.com>
2025-09-18 20:00:32 +0530

    Update TaskDao.java

--- lab7.3-Spring-Boot-JPA-Rest-Repository/src/main/java/demo/boot/dao/TaskDao.java
+++ lab7.3-Spring-Boot-JPA-Rest-Repository/src/main/java/demo/boot/dao/TaskDao.java
@@ -2,6 +2,8 @@ package demo.boot.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -14,8 +16,21 @@ import demo.boot.model.Task;
 public interface TaskDao  extends CrudRepository<Task, Long>{
 
 	
-	  public List<Task> findTaskByDescription(@Param("desc") String description);
-	 
+	//  http://localhost:8080/task/search/findTaskByDescription?desc=YOUR_DESCRIPTION
+    // @Query("SELECT t FROM Task t WHERE t.description = :desc")
+    List<Task> findTaskByDescription(@Param("desc") String description);
     
+    // http://localhost:8080/task/search/findTasksByDescriptionContains?desc=code
+    @Query("SELECT t FROM Task t WHERE t.description LIKE %:desc%")
+    List<Task> findTasksByDescriptionContains(@Param("desc") String description);
+	 
+    // http://localhost:8080/task/search/countTasksByDescription?desc=YOUR_DESCRIPTION
+    @Query("SELECT COUNT(t) FROM Task t WHERE t.description = :desc")
+    long countTasksByDescription(@Param("desc") String description);
+    
+    @Query("SELECT t FROM Task t WHERE t.description LIKE %:desc% ORDER BY t.description ASC")
+    List<Task> findTasksByDescriptionSorted(@Param("desc") String description);
+    
+
 }
 
